86729828609fb904b19b00522d4977378b59f496
karishmask46 <karishmask.15999@gmail.com>
2022-08-03 19:05:26 +0530

    Adding First name

---
+++ UserRegistration/Pattern.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistration
+{
+     class Patterns
+    {
+        public static string RegexName = "^[A-Z][a-z]{3,}$";
+
+        public bool validateString(string sample)
+        {
+            return Regex.IsMatch(sample, RegexName);
+        }
+    }
+}
---
+++ UserRegistration/Program.cs
@@ -0,0 +1,10 @@
+﻿using UserRegistration;
+Patterns patterns= new Patterns();
+if (patterns.validateString("Shaik"))
+
+{
+    Console.WriteLine("Valid");
+}
+else
+    Console.WriteLine("Invalid");
+Console.ReadKey();
